72dee6147b0d20117f8416475557d346774f8d6f
Zerrouati Nabila <nabilafaiza38@gmail.com>
2021-06-30 00:43:06 +0100

    section 11_end

--- MySrc/App.js
+++ MySrc/App.js
@@ -1,9 +1,10 @@
-import { useState } from 'react';
 
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import Cart from './components/Cart/Cart';
-import CartProvider from './store/CartProvider';
+ 
+import { Fragment, useState } from 'react';
+
+import Header from './Components/Layout/Hearder';
+import Meals from './Components/Meals/Meals';
+import Cart from './Components/Cart/Cart';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -17,13 +18,13 @@ function App() {
   };
 
   return (
-    <CartProvider>
+    <Fragment>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </CartProvider>
+    </Fragment>
   );
 }
 
